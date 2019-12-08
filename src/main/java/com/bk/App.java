b90fd35159b9f6949c2eac6497e12745eebc8b9f
bharatkapse <bharat.kapse@gmail.com>
2019-12-08 20:32:26 +0530

    done small changes

--- src/main/java/com/bk/App.java
+++ src/main/java/com/bk/App.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Bharat!");
         Scanner sc = null;
         try {
             sc = new Scanner(System.in);
@@ -22,17 +22,17 @@ public class App {
         StudyManager stdm = new StudyManager();
 
         while (!choice.equalsIgnoreCase("quit")) {
-            String filename = "studyreport.csv";
+            String filename = "studied.csv";
             System.out.println("Enter content type");
             System.out.println("1.studied\t 2.reffered");
             String contentType = sc.nextLine();
             if (contentType.contains("2")) {
-                filename = "refferedreport.csv";
+                filename = "reffered.csv";
             }
 
             System.out.println("Enter your choice");
-            System.out.println("1.Add data to csv file");
-            System.out.println("2.Display history of added data");
+            System.out.println("1.Add data to record");
+            System.out.println("2.show previous records");
             choice = sc.nextLine();
             if (choice.contains("1")) {
                 stdm.initialize(filename, "write");
@@ -41,7 +41,7 @@ public class App {
                 while (ch.equalsIgnoreCase("yes")){
                     String[] data = stdm.readData(filename);
                     stdm.addToCSV(filename, data);
-                    System.out.println("Do you want to add more info (yes/no)");
+                    System.out.println("Do you want to add more records (yes/no)");
                     ch= sc.nextLine();
                     if(!ch.contains("no")){
                         ch="yes";
@@ -53,7 +53,7 @@ public class App {
                 stdm.initialize(filename, "read");
                 stdm.showCSVContent(filename);
             }
-            System.out.println("press any key to continue... type quit to exit");
+            System.out.println("press any key to continue..... type quit to exit");
             choice= sc.nextLine();
         }
     }
